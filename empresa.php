03259b3578ccfa5aab1101a5e77ac12e452ebd8e
bguzman2004 <167243617+bguzman2004@users.noreply.github.com>
2026-08-17 19:32:01 -0400

    Cambios empresa.php

--- empresa.php
+++ empresa.php
@@ -12,7 +12,9 @@
         <!-- Navbar -->
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">Logo</a>
+                <a class="navbar-brand" href="index.php">
+                    <img src="img/logo.jpg" alt="Logo" height="60">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -55,7 +57,7 @@
             <div class="row">
                 <div class="col-4"></div>
                 <div class="col-4 d-flex justify-content-center" 
-                style="color:white"><strong>MiEmpresa@2026</strong></div>
+                style="color:white"><strong></strong></div>
                 <div class="col-4"></div>
             </div>
         </div>
@@ -73,24 +75,24 @@
                     <div class="modal-body">
                         <form action="empresa.php">
                             <div class="mb-2 mt-2">
-                                <label for="email" class="form-label">Email:</label>
-                                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                                <label for="email" class="form-label">Correo:</label>
+                                <input type="email" class="form-control" id="email" placeholder="Ingresar Correo " name="email">
                             </div>
                             <div class="mb-2">
-                                <label for="pwd" class="form-label">Password:</label>
-                                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                                <label for="pwd" class="form-label">Contraseña:</label>
+                                <input type="password" class="form-control" id="pwd" placeholder="Ingresar Contraseña" name="pswd">
                             </div>
                             <div class="form-check mb-2">
                                 <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" name="remember"> Remember me
+                                <input class="form-check-input" type="checkbox" name="Recordarme"> Recordarme
                                 </label>
                             </div>
-                            <button type="submit" class="btn btn-primary">Login  <i class="fa fa-check-circle"></i></button>
+                            <button type="submit" class="btn btn-primary">Ingresar <i class="fa fa-check-circle"></i></button>
                         </form>
                     </div>
                     <!-- Modal footer -->
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
                     </div>
                 </div>
             </div>
